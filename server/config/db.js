4e5f14aef953facf76bbf6d804fd5b3971b71f5c
devs552 <abdullah@gmail.com>
2025-03-07 08:54:36 +0500

    new push

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     console.log(`📡 Connecting to MongoDB at: ${process.env.MONGO_URI}`);
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}:${conn.connection.port} (${conn.connection.name})`);
 
