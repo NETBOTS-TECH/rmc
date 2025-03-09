b54e7ee67bb028fe96cf0e5e2253d3f09de07d76
devs552 <abdullah@gmail.com>
2025-03-09 23:56:10 +0500

    another

--- server/server.js
+++ server/server.js
@@ -7,34 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const allowedOrigins = [
-  "https://repairmyconcrete.com",
-  "http://localhost:3000",   // Local development
-  "http://82.29.179.48:3000"
-];
+app.use(cors({
+  origin: ["https://repairmyconcrete.com", "http://localhost:3000", "http://82.29.179.48:3000"],
+  credentials: true
+}));
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-};
-
-// Apply CORS for all routes
-app.use(cors(corsOptions));
-app.options("/api/services", cors(corsOptions));
-app.options("/api/estimates", cors(corsOptions));
-app.options("/api/contacts", cors(corsOptions));
-app.options("/api/gallery", cors(corsOptions));
-app.options("/api/chat-user", cors(corsOptions));
-app.options("/api/blogs", cors(corsOptions));
-app.options("/auth", cors(corsOptions));
 
 app.use(express.json());
 
