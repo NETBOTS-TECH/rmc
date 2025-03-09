652ebdd6bcc34823c614a71ef27aeb32a7ffb346
devs552 <abdullah@gmail.com>
2025-03-10 00:08:24 +0500

    lets

--- server/server.js
+++ server/server.js
@@ -7,11 +7,28 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: ["https://repairmyconcrete.com", "http://localhost:3000", "http://82.29.179.48:3000"],
-  credentials: true
-}));
+const allowedOrigins = [
+  "https://repairmyconcrete.com",
+  "http://localhost:3000",  // For local development
+  "http://82.29.179.48:3000"
+];
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin); // Set specific origin
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 app.use(express.json());
 
