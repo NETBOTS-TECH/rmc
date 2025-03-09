a470437d1b013e0f3738c51e2cc4d2e0adf28436
devs552 <abdullah@gmail.com>
2025-03-09 23:31:28 +0500

    cross policy fix

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
